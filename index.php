dc0d01203d11ec2455c02b533086859b7b4652e0
Malakahh <Malakahh@users.noreply.github.com>
2016-08-14 01:50:52 +0200

    Added image slider. Wrote content for Object pool.

--- index.php
+++ index.php
@@ -198,18 +198,22 @@
 						</a>
 					</div>
 					<div class="col-md-4 portfolioItem">
-						<a href="#scoutsModal" class="portfolio-link" data-toggle="modal">
+						<a href="#objectPoolModal" class="portfolio-link" data-toggle="modal">
 							<div class="portfolioClickable">
 								<h2>
-									Portfolio Item Headline
+									Object Pool
 								</h2>
 								<p>
-									Our hardware and software modficiations demonstrate that simulating Dika is one thing,but deploying it in the wild is a completely different story.
-									That being said, we ran four novel experiments: (1) we ran 64 trials with a simulated RAID array workload, and compared results to our software simulation;
-									(2) we measured RAM throughput as a function of RAM speed on a Commodore 64;
-									(3) we dogfooded Dika on our own desktop machines, paying particular attention to interrupt rate;
-									and (4) we ran 70 trials with a simulated instant messenger workload, and compared results to our hardware emulation.
-									We discarded the results of some earlier experiments, notably when we measured RAM speed as a function of ROM space on an UNIVAC.
+									<ul>
+										<li>Fully documented</li>
+										<li>Contains any object</li>
+										<li>Grows by N*2, where N = number of requests, ensuring readily available supply</li>
+										<li>Asynchronous instantiation</li>
+										<li>Singleton pattern</li>
+										<li>Unit tests</li>
+										<li>Tested to improve performance by 42%</li>
+										<li>Developed for Unity3D</li>
+									</ul>
 								</p>
 							</div>
 						</a>
@@ -223,15 +227,14 @@
 						<a href="#scoutsModal" class="portfolio-link" data-toggle="modal">
 							<div class="portfolioClickable">
 								<h2>
-									Portfolio Item Headline
+									Data structure: Heap
 								</h2>
 								<p>
-									Our hardware and software modficiations demonstrate that simulating Dika is one thing,but deploying it in the wild is a completely different story.
-									That being said, we ran four novel experiments: (1) we ran 64 trials with a simulated RAID array workload, and compared results to our software simulation;
-									(2) we measured RAM throughput as a function of RAM speed on a Commodore 64;
-									(3) we dogfooded Dika on our own desktop machines, paying particular attention to interrupt rate;
-									and (4) we ran 70 trials with a simulated instant messenger workload, and compared results to our hardware emulation.
-									We discarded the results of some earlier experiments, notably when we measured RAM speed as a function of ROM space on an UNIVAC.
+									<ul>
+										<li>Implemented using a binary tree</li>
+										<li>Both min- and max heap</li>
+										<li>Generic</li>
+									</ul>
 								</p>
 							</div>
 						</a>
@@ -240,15 +243,14 @@
 						<a href="#scoutsModal" class="portfolio-link" data-toggle="modal">
 							<div class="portfolioClickable">
 								<h2>
-									Portfolio Item Headline
+									Algorithm: A* Search
 								</h2>
 								<p>
-									Our hardware and software modficiations demonstrate that simulating Dika is one thing,but deploying it in the wild is a completely different story.
-									That being said, we ran four novel experiments: (1) we ran 64 trials with a simulated RAID array workload, and compared results to our software simulation;
-									(2) we measured RAM throughput as a function of RAM speed on a Commodore 64;
-									(3) we dogfooded Dika on our own desktop machines, paying particular attention to interrupt rate;
-									and (4) we ran 70 trials with a simulated instant messenger workload, and compared results to our hardware emulation.
-									We discarded the results of some earlier experiments, notably when we measured RAM speed as a function of ROM space on an UNIVAC.
+									<ul>
+										<li>Informed search using a provided heuristic</li>
+										<li>Includes graph data structure with generic nodes and edges</li>
+										<li>Frontier based on a binary heap</li>
+									</ul>
 								</p>
 							</div>
 						</a>
@@ -293,10 +295,139 @@
 								<div class="rl"></div>
 							</div>
 						</div>
+						
+
+						<!-- CONTENT -->
 						<img class="modalImgHeadline" src="Assets/scouts/scoutsTitle.png">
 						<p>Join Billy Bullseye, Tina Ties-A-Knot, and Chubby Chuck on their scouting adventures.
 							Collect plants, spot animals and navigate obstacles, but watch out for bears...</p>
 						<p>A more detailed text...</p>
+
+						<!-- IMAGE SLIDER -->
+						<div id="scoutsSlider" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+						 	<ol class="carousel-indicators">
+								<li data-target="#scoutsSlider" data-slide-to="0" class="active"></li>
+								<li data-target="#scoutsSlider" data-slide-to="1"></li>
+								<li data-target="#scoutsSlider" data-slide-to="2"></li>
+							</ol>
+
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+								<div class="item active">
+									<img src="Assets/ConstructionPenguin.png">
+									<div class="carousel-caption">
+										Example caption
+									</div>
+								</div>
+								<div class="item">
+									<img src="Assets/ConstructionPenguin.png">
+									<div class="carousel-caption">
+										Another example caption
+									</div>
+								</div>
+							</div>
+
+							<!-- Controls -->
+							<a class="left carousel-control" href="#scoutsSlider" role="button" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#scoutsSlider" role="button" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+
+
+						<div class="row">
+							<div class="col-xs-4 col-md-5"></div>
+							<div class="col-xs-4 col-md-2">
+								<button type="button" class="btn btn-default modalBtnClose" data-dismiss="modal">Close</button>
+							</div>
+							<div class="col-xs-4 col-md-5"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="objectPoolModal" class="modal fade" tabindex="-1" role="dialog">
+			<div class="modal-dialog modal-lg" role="document">
+				<div class="modal-content">
+					<div class="modal-body">
+						<div class="close-modal" data-dismiss="modal">
+							<div class="lr">
+								<div class="rl"></div>
+							</div>
+						</div>
+
+						<!-- CONTENT -->
+						<h1>Object Pool</h1>
+						<p>
+							The purpose of an object pool is essentially enabling recycling of objects, and it is done for performance reasons.
+						</p>
+						<p>
+							The performance of the object pool has been benchmarked to be 42.08% faster than without using the object pool. The testing procedure were as follows: Instantiate/acquire a number of objects, and destroy/return every second one. Run 10 tests, and average the results.
+						</p>
+						<p>
+							This object pool is developed in C# for Unity3D. It features async instantiation of any objects given a threshold. It will grow by N*2, where N = number of requests, ensuring a readily available supply of objects. Furthermore, it follows the Singleton pattern.
+						</p>
+						<p>
+							Alongside the object pool, you will find unit tests as well as fully commented code and documentation to get started using the API.
+						</p>
+
+						<div class="portfolioButtons">
+							<p>
+								You can find the source code at:
+							</p>
+							<a target="_blank" href="https://github.com/Malakahh/MalasObjectPool">
+								<img class="findMeOnTheWebButtonImages" src="Assets\GitHub_Logo.svg">
+							</a>
+						</div>						
+
+						<!-- IMAGE SLIDER -->
+						<div id="objectPoolSlider" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+						 	<ol class="carousel-indicators">
+								<li data-target="#objectPoolSlider" data-slide-to="0" class="active"></li>
+								<li data-target="#objectPoolSlider" data-slide-to="1"></li>
+								<li data-target="#objectPoolSlider" data-slide-to="2"></li>
+							</ol>
+
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+								<div class="item active">
+									<img src="Assets/objectPool/comments.png">
+									<div class="carousel-caption">
+										An example of XML documentation comments found in this project
+									</div>
+								</div>
+								<div class="item">
+									<img src="Assets/objectPool/documentation.png">
+									<div class="carousel-caption">
+										The README file found in the project. In contains full API documentaion as well as a usage example and quickstart guide
+									</div>
+								</div>
+								<div class="item">
+									<img src="Assets/objectPool/packageContents.png">
+									<div class="carousel-caption">
+										This picture shows the content of the Unity3D package that this object pool is delivered as
+									</div>
+								</div>
+							</div>
+
+							<!-- Controls -->
+							<a class="left carousel-control" href="#objectPoolSlider" role="button" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#objectPoolSlider" role="button" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+
 						<div class="row">
 							<div class="col-xs-4 col-md-5"></div>
 							<div class="col-xs-4 col-md-2">
