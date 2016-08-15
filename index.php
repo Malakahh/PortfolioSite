d1564fc0eef458fffad0ff2d9f875caee479f5ab
Malakahh <Malakahh@users.noreply.github.com>
2016-08-15 18:10:18 +0200

    Added Hotshot Showoff content

--- index.php
+++ index.php
@@ -135,38 +135,42 @@
 					<div class="row videoOverlayHeadline">
 						<div class="col-xs-2 col-md-4"></div>
 						<div class="col-xs-8 col-md-4">
-							<img class="scoutsTitle" src="Assets/scouts/scoutsTitle.png">
+							<img class="hotshotShowoffTitle" src="Assets/hotshotShowoff/hotshotShowoffLogo.png">
 						</div>
 						<div class="col-xs-2 col-md-4"></div>
 					</div>
 				</div>
-				<video class="videoBackground" src="Assets/scouts/scouts.mp4" poster="Assets/scouts/scoutsFirstFrame.png" preload="none" controls autoplay loop muted></video>
+				<video class="videoBackground" src="Assets/hotshotShowoff/hotshotShowoff.mp4" poster="Assets/hotshotShowoff/hotshotShowoffFirstFrame.png" preload="none" controls autoplay loop muted></video>
 				<div class="row videoOverlayContent">
 					<div class="col-xs-1 col-md-2"></div>
 					<div class="col-xs-10 col-md-8">
-						<p>
-							Yet another game...
-						</p>
+						<ul>
+							<li>Developed at a gamejam at DADIU</li>
+							<li>September 2014</li>
+							<li>
+								Responsibilities included:
+								<ul>
+									<li>Capturing requirements from game designers and level designers</li>
+									<li>C#</li>
+									<li>Gameplay logic</li>
+								</ul> 
+							</li>
+						</ul>
 						<p>	
 							<div class="row">
 								<div class="col-md-2 portfolioButtons">
-									<a target="_blank" href="https://play.google.com/store/apps/details?id=dk.dadiu.scouts&hl=da">
-										<img src="Assets\Get_it_on_Google_play.svg">
+									<a target="_blank" href="downloads/Hotshotshowoff.apk">
+										<img src="Assets\apk.svg">
 									</a>
 								</div>
 								<div class="col-md-2 portfolioButtons">
-									<a target="_blank" href="https://itunes.apple.com/us/app/scouts/id958947631?mt=8&ign-mpt=uo%3D4">
-										<img src="Assets\Available_on_the_App_Store_(black)_SVG.svg">
-									</a>
-								</div>
-								<div class="col-md-2 portfolioButtons">
-									<a target="_blank" href="http://www.example.com">
+									<a target="_blank" href="downloads/HotshotShowoff.zip">
 										<img src="Assets\Windows_logo_-_2012.svg">
 									</a>
 								</div>
-								<div class="col-md-4 portfolioButtons"></div>
+								<div class="col-md-6 portfolioButtons"></div>
 								<div class="col-md-2 portfolioButtons">
-									<button type="button" class="btn btn-default btn-lg" data-toggle="modal" data-target="#scoutsModal">Read more</button>
+									<button type="button" class="btn btn-default btn-lg" data-toggle="modal" data-target="#hotshotShowoffModal">Read more</button>
 								</div>
 							</div>
 						</p>
@@ -389,6 +393,105 @@
 			</div>
 		</div>
 
+		<div id="hotshotShowoffModal" class="modal fade" tabindex="-1" role="dialog">
+			<div class="modal-dialog modal-lg" role="document">
+				<div class="modal-content">
+					<div class="modal-body">
+						<div class="close-modal" data-dismiss="modal">
+							<div class="lr">
+								<div class="rl"></div>
+							</div>
+						</div>
+						
+						<!-- CONTENT -->
+						<img class="modalImgHeadline" src="Assets/hotshotShowoff/hotshotShowoffLogo.png">
+						<p>
+							In the wild west there can be only one greatest gunslinger...
+						</p>
+						<p>
+							Hotshot Showoff was made at a 2 day gamejam during my stay at DADIU from September 2014 - December 2014. It is designed mainly with touch-screen devices in mind.
+						</p>
+						<p>
+							The game is developed in C# using the Unity3D game engine. For this game, I developed the scoring system in close collaboration with the game designer. Tied in to the scoring and combo system the other main system I was working on, namely the progressive spawning of audience members, where I collaborated with the level designer.
+						</p>
+
+						<div class="portfolioButtons">
+							<p>
+								To watch the game trailer follow the link below
+							</p>
+							<div class="row">
+								<div class="col-md-5"></div>
+								<div class="col-md-2 portfolioButtons">
+									<a target="_blank" href="https://www.youtube.com/watch?v=EDdloaRRb7g">
+										<img class="findMeOnTheWebButtonImages" src="Assets\YouTube_logo.svg">
+									</a>
+								</div>
+								<div class="col-md-5"></div>
+							</div>
+						</div>
+
+						<!-- IMAGE SLIDER -->
+						<div id="hotshotShowoffSlider" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+						 	<ol class="carousel-indicators">
+								<li data-target="#hotshotShowoffSlider" data-slide-to="0" class="active"></li>
+								<li data-target="#hotshotShowoffSlider" data-slide-to="1"></li>
+								<li data-target="#hotshotShowoffSlider" data-slide-to="2"></li>
+								<li data-target="#hotshotShowoffSlider" data-slide-to="3"></li>
+							</ol>
+
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+								<div class="item active">
+									<img src="Assets/hotshotShowoff/gameplay.png">
+									<div class="carousel-caption">
+										Gameplay sample. Currently 2 out of 4 cans are in play. Notice the growing audience
+									</div>
+								</div>
+								<div class="item">
+									<img src="Assets/hotshotShowoff/Bg_Night.png">
+									<div class="carousel-caption">
+										Concept art of a night time setting
+									</div>
+								</div>
+								<div class="item">
+									<img src="Assets/hotshotShowoff/Cowboygame_bg.png">
+									<div class="carousel-caption">
+										Backdrop to the gameplay
+									</div>
+								</div>
+								<div class="item">
+									<img src="Assets/hotshotShowoff/credits.png">
+									<div class="carousel-caption">
+										Credits screen
+									</div>
+								</div>
+							</div>
+
+							<!-- Controls -->
+							<a class="left carousel-control" href="#hotshotShowoffSlider" role="button" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#hotshotShowoffSlider" role="button" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+
+
+						<div class="row">
+							<div class="col-xs-4 col-md-5"></div>
+							<div class="col-xs-4 col-md-2">
+								<button type="button" class="btn btn-default modalBtnClose" data-dismiss="modal">Close</button>
+							</div>
+							<div class="col-xs-4 col-md-5"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
 		<div id="objectPoolModal" class="modal fade" tabindex="-1" role="dialog">
 			<div class="modal-dialog modal-lg" role="document">
 				<div class="modal-content">
@@ -543,11 +646,11 @@
 							The heuristic is problem specific. The algorithm is guaranteed to return the shortest path if the heuristic never provides an overestimate.
 						</p>
 						<p>
-							Here, I have implemented a generic directed, weighted graph data structure to traverse. To use this algorithm for pathfinding, the world space should be represented as this type of graph.
+							In this project I have implemented a generic directed, weighted graph data structure to traverse. To use this algorithm for pathfinding, the world space should be represented as this type of graph.
 							Furthermore, to choose the next node to explore I use a <i>min-heap</i> as the frontier. This makes acquiring the next node with the lowest cost cheap.
 						</p>
 						<p>
-							In the examples below, the start node is marked green, the goal node is marked red, and the shortest path as found by the algorithm is coloured blue.
+							In the examples below, the start node is marked green, the goal node is marked red, and the shortest path, as found by this implementation, is coloured blue.
 						</p>
 
 						<div class="portfolioButtons">
