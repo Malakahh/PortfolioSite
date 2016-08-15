0b66b4c0866dc9b24a15ed2e6a5786c49db23b8b
Malakahh <Malakahh@users.noreply.github.com>
2016-08-15 13:43:47 +0200

    Added content for A*

--- index.php
+++ index.php
@@ -202,7 +202,7 @@
 						</a>
 					</div>
 					<div class="col-md-4 portfolioItem">
-						<a href="#scoutsModal" class="portfolio-link" data-toggle="modal">
+						<a href="#heapModal" class="portfolio-link" data-toggle="modal">
 							<div class="portfolioClickable">
 								<h2>
 									Heap
@@ -211,6 +211,7 @@
 									<ul>
 										<li>Implemented using a binary tree</li>
 										<li>Both min- and max heap</li>
+										<li>Documented code</li>
 										<li>Generic</li>
 									</ul>
 								</p>
@@ -223,7 +224,7 @@
 				<div class="row">
 					<div class="col-md-2"></div>
 					<div class="col-md-4 portfolioItem">
-						<a href="#scoutsModal" class="portfolio-link" data-toggle="modal">
+						<a href="#AStarModal" class="portfolio-link" data-toggle="modal">
 							<div class="portfolioClickable">
 								<h2>
 									A* Search
@@ -231,7 +232,7 @@
 								<p>
 									<ul>
 										<li>Informed search using a provided heuristic</li>
-										<li>Includes graph data structure with generic nodes and edges</li>
+										<li>Includes directed graph data structure with generic nodes and edges</li>
 										<li>Frontier based on a binary heap</li>
 									</ul>
 								</p>
@@ -459,6 +460,142 @@
 								<span class="sr-only">Previous</span>
 							</a>
 							<a class="right carousel-control" href="#objectPoolSlider" role="button" data-slide="next">
+								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+								<span class="sr-only">Next</span>
+							</a>
+						</div>
+
+						<div class="row">
+							<div class="col-xs-4 col-md-5"></div>
+							<div class="col-xs-4 col-md-2">
+								<button type="button" class="btn btn-default modalBtnClose" data-dismiss="modal">Close</button>
+							</div>
+							<div class="col-xs-4 col-md-5"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="heapModal" class="modal fade" tabindex="-1" role="dialog">
+			<div class="modal-dialog modal-lg" role="document">
+				<div class="modal-content">
+					<div class="modal-body">
+						<div class="close-modal" data-dismiss="modal">
+							<div class="lr">
+								<div class="rl"></div>
+							</div>
+						</div>
+
+						<!-- CONTENT -->
+						<h1>Heap</h1>
+
+
+						<p>
+							A heap is a tree-based structure. This heap is implemented as described in CLRS, and is a binary heap.	
+						</p>
+						<p>
+							It has support to be two kinds of heap, a <i>max-heap</i> and a <i>min-heap</i>. Both satisfy the <i>heap property</i>.
+							It follow that if we have an array <i>A</i>, the max-heap property is that for every node in A (except the root node) the parent is larger than both its children. In a min-heap, the parent node is smaller than both its children.
+						</p>
+						<p>
+							This makes heaps a valuable data structure in several algorithms such as Dijkstra's algorithm or A* search.
+						</p>
+
+						<div class="portfolioButtons">
+							<p>
+								You can find the source code at:
+							</p>
+							<a target="_blank" href="https://github.com/Malakahh/Heap">
+								<img class="findMeOnTheWebButtonImages" src="Assets\GitHub_Logo.svg">
+							</a>
+						</div>	
+
+						<div class="row">
+							<div class="col-xs-4 col-md-5"></div>
+							<div class="col-xs-4 col-md-2">
+								<button type="button" class="btn btn-default modalBtnClose" data-dismiss="modal">Close</button>
+							</div>
+							<div class="col-xs-4 col-md-5"></div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div id="AStarModal" class="modal fade" tabindex="-1" role="dialog">
+			<div class="modal-dialog modal-lg" role="document">
+				<div class="modal-content">
+					<div class="modal-body">
+						<div class="close-modal" data-dismiss="modal">
+							<div class="lr">
+								<div class="rl"></div>
+							</div>
+						</div>
+
+						<!-- CONTENT -->
+						<h1>A* Search</h1>
+						<p>
+							A* search is an informed search using a provided heuristic. It is commonly used for graph traversal and pathfinding.
+							It works on a directed, weighted graph, constantly expanding its frontier as a lowest-cost-first search, using the heuristic to estimate the remaining cost to reach the goal.
+						</p>
+						<p>
+							The heuristic is problem specific. The algorithm is guaranteed to return the shortest path if the heuristic never provides an overestimate.
+						</p>
+						<p>
+							Here, I have implemented a generic directed, weighted graph data structure to traverse. To use this algorithm for pathfinding, the world space should be represented as this type of graph.
+							Furthermore, to choose the next node to explore I use a <i>min-heap</i> as the frontier. This makes acquiring the next node with the lowest cost cheap.
+						</p>
+						<p>
+							In the examples below, the start node is marked green, the goal node is marked red, and the shortest path as found by the algorithm is coloured blue.
+						</p>
+
+						<div class="portfolioButtons">
+							<p>
+								You can find the source code at:
+							</p>
+							<a target="_blank" href="https://github.com/Malakahh/A-star">
+								<img class="findMeOnTheWebButtonImages" src="Assets\GitHub_Logo.svg">
+							</a>
+						</div>						
+
+						<!-- IMAGE SLIDER -->
+						<div id="AStarSlider" class="carousel slide" data-ride="carousel">
+							<!-- Indicators -->
+						 	<ol class="carousel-indicators">
+								<li data-target="#AStarSlider" data-slide-to="0" class="active"></li>
+								<li data-target="#AStarSlider" data-slide-to="1"></li>
+								<li data-target="#AStarSlider" data-slide-to="2"></li>
+							</ol>
+
+							<!-- Wrapper for slides -->
+							<div class="carousel-inner" role="listbox">
+								<div class="item active">
+									<img src="Assets/A-star/graph.png">
+									<div class="carousel-caption">
+										A directed, weighted graph.
+									</div>
+								</div>
+								<div class="item">
+									<img src="Assets/A-star/map.png">
+									<div class="carousel-caption">
+										A simple map. Each cell is a node, with edges to all 8 neighbouring cells. Obstructed nodes are coloured black.
+									</div>
+								</div>
+								<div class="item">
+									<img src="Assets/A-star/maze.png">
+									<div class="carousel-caption">
+										A maze-style map. Each cell is a node, with edges to all vertical and horizontal neighbouring cells. Obstructed nodes are coloured black.
+									</div>
+								</div>
+							</div>
+
+							<!-- Controls -->
+							<a class="left carousel-control" href="#AStarSlider" role="button" data-slide="prev">
+								<span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+								<span class="sr-only">Previous</span>
+							</a>
+							<a class="right carousel-control" href="#AStarSlider" role="button" data-slide="next">
 								<span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
 								<span class="sr-only">Next</span>
 							</a>
